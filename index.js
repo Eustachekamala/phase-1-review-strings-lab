c9b2dc627b8108d46a613d831402526be4f7ed62
eustachekamala <eustachejared25@gmail.com>
2024-06-30 18:35:28 +0300

    I done the assignment that we asks us to declarate somme variables

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper';
+
+let welcomeMessage = currentUser + 'Welcome to Flatbook, !'
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()+', '+ currentUser+'!';
+
+let shortGreeting = 'Welcome, '+ 'Grace!';
